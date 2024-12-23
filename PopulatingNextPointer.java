865574132008ff64baff32a35881b672c8f94a38
firoz Syed <syedfiroz2010@gmail.com>
2024-12-23 13:42:53 -0800

    update

--- PopulatingNextPointer.java
+++ PopulatingNextPointer.java
@@ -183,3 +183,48 @@ class Solution {
         helper(curr.right);
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+// Approach : Using DFS but applying the logic of symmetric tree.
+// Standing at children and we form connections directly.
+// Time : O(n)
+// Space : O(h) # recursive stack space
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        helper(root.left,root.right);
+        return root;
+    }
+    public void helper(Node left,Node right){
+        // base case
+        if(left == null) return;
+
+        // logic
+        left.next = right;
+        // recursion
+        helper(left.left,left.right);
+        helper(left.right,right.left);
+        helper(right.left,right.right);
+    }
+}
